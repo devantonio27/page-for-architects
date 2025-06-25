5f5bac9dc7361613ecf55f44059c449842b80663
Tonidev <antoniodev2709@gmail.com>
2025-06-25 09:32:45 -0300

    create menu mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,12 +2,16 @@ import Banner from "../components/Banner/Banner";
 import Button from "../components/Button/Button";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer.jsx/Footer";
+import ContactForm from "../components/ContactForm/ContactForm";
 
 function Contact() {
   return (
     <>
       <Header />
       <Banner title="Contact" image="contact.jpg" />
+      <div className="container">
+        <ContactForm />
+      </div>
       <Footer />
     </>
   );
