7a0b68634ea704958b09b4481b66fdd60f48c138
Tonidev <antoniodev2709@gmail.com>
2025-06-25 09:50:21 -0300

    add utils ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-//PAGES
+// PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 
+// UTILS
+import ScrollToTop from "./utils/ScrollTop";
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
