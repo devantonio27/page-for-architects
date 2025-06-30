1d53aa8a0c8051649f84d23ab4b8465df715956a
Tonidev <antoniodev2709@gmail.com>
2025-06-30 09:20:19 -0300

    add get projects in Api services

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
 import "./ProjectsList.css";
 
 //ASSETS
 import Like from "../../assets/like.svg";
 import LikedFilled from "../../assets/like-filled.svg";
 
+//UTILS
+import { getApiData } from "../../Services/apiServices";
+
 function ProjectsList() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData("projects");
+        setProjects(projectsResponse);
+      } catch {
+        setProjects([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="projects-section">
@@ -17,54 +36,20 @@ function ProjectsList() {
           </p>
         </div>
         <div className="projects-grid">
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
-          <div className="project-card d-flex jc-center al-center fd-column">
-            <div className="thumb tertiary-background"></div>
-            <h3>antonio</h3>
-            <p>Maceio-AL</p>
-            <img src={Like} alt="" height="20px" />
-          </div>
+          {projects.map((project) => (
+            <div
+              className="project-card d-flex jc-center al-center fd-column"
+              key={project.id}
+            >
+              <div
+                className="thumb tertiary-background"
+                style={{ backgroundImage: `url(${project.thumb})` }}
+              ></div>
+              <h3>{project.title}</h3>
+              <p>{project.subtitle}</p>
+              <img src={Like} alt="" height="20px" />
+            </div>
+          ))}
         </div>
       </div>
     </>
