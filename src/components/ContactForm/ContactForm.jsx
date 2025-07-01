1b34119d926539ec29a03eecc647d2809eeb3076
Tonidev <antoniodev2709@gmail.com>
2025-07-01 10:02:48 -0300

    feat(contactForm): validation contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,6 +5,43 @@ import "./ContactForm.css";
 import Button from "../Button/Button";
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      null;
+    }
+  };
+
+  useEffect(() => {
+    const isValidEmail = (email) => {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(email);
+    };
+
+    const isValid =
+      formData.name.trim() &&
+      formData.email.trim() &&
+      isValidEmail(formData.email) &&
+      formData.message.trim();
+
+    setIsFormValid(isValid);
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <>
       <div className="contact-form d-flex fd-column al-center">
@@ -17,6 +54,7 @@ function ContactForm() {
               id="name"
               name="name"
               placeholder="Name *"
+              onChange={handleChange}
             />
             <input
               type="email"
@@ -24,6 +62,7 @@ function ContactForm() {
               id="email"
               name="email"
               placeholder="Email *"
+              onChange={handleChange}
             />
           </div>
 
@@ -34,10 +73,15 @@ function ContactForm() {
               name="message"
               placeholder="Mensagem *"
               rows="4"
+              onChange={handleChange}
             ></textarea>
           </div>
           <div className="al-center d-flex jc-end form-group">
-            <Button type="submit" buttonStyle="secondary">
+            <Button
+              type="submit"
+              buttonStyle="secondary"
+              disabled={!isFormValid}
+            >
               Enviar
             </Button>
           </div>
