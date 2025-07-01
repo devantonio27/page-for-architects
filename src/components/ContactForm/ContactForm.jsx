12bc5ac969382f08196f58be1f44399fccc7f9a7
Tonidev <antoniodev2709@gmail.com>
2025-07-01 09:14:44 -0300

    feat: create contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,47 @@
+import { useState, useEffect } from "react";
 import "./ContactForm.css";
+
+//COMPONENTS
+import Button from "../Button/Button";
 
 function ContactForm() {
   return (
     <>
       <div className="contact-form d-flex fd-column al-center">
         <h2>We love meeting new people and helping them.</h2>
-        <form>WIP...</form>
+        <form>
+          <div className="d-flex form-group">
+            <input
+              type="text"
+              className="form-input"
+              id="name"
+              name="name"
+              placeholder="Name *"
+            />
+            <input
+              type="email"
+              className="form-input"
+              id="email"
+              name="email"
+              placeholder="Email *"
+            />
+          </div>
+
+          <div className="d-flex form-group">
+            <textarea
+              className="form-input"
+              id="message"
+              name="message"
+              placeholder="Mensagem *"
+              rows="4"
+            ></textarea>
+          </div>
+          <div className="al-center d-flex jc-end form-group">
+            <Button type="submit" buttonStyle="secondary">
+              Enviar
+            </Button>
+          </div>
+        </form>
       </div>
     </>
   );
